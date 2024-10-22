69ae30dbac2c5f8c37b95413dd4c65a502587eee
ignacioTech <ignaciobarboza1983@gmail.com>
2024-10-22 09:49:34 -0300

    Iniciacion
    Conceptos básicos

---
+++ 1_tipos_Datos.py
@@ -0,0 +1,13 @@
+#Enteros(int)
+NumEntero = 10
+
+#Flotante-Decimal (Float)
+NumFloat = 10.5
+
+#Cadena de texto (String)
+Texto = "Cadena de texto"
+
+#Booleanos (los valores deben comenzar con mayuscula)
+verdadero= True
+falso = False
+
---
+++ 2_operadores.py
@@ -0,0 +1,15 @@
+#Aritmeticos
+
+suma = 5 + 5
+resta = 7 - 2
+division = 20 / 2
+divisionEntera = 20 // 3
+modulo = 20 % 3 
+exponeniacion = 2 ** 4
+
+print("Suma 5 + 5 = " , suma)
+print("Resta 7 - 2 = " , resta)
+print("Division 20 / 2 = " , division)
+print("Division Entera 20 // 3 = " , divisionEntera)
+print("Modulo 20 % 3 = " , modulo)
+print("Exponenciacion 2 ** 4 = " , exponeniacion)
