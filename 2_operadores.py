7abbbaf71e6504698d5c4b35826e68b09e418071
ignacioTech <ignaciobarboza1983@gmail.com>
2024-10-23 09:19:25 -0300

    Update 2_operadores.py

--- 2_operadores.py
+++ 2_operadores.py
@@ -7,9 +7,29 @@ divisionEntera = 20 // 3
 modulo = 20 % 3 
 exponeniacion = 2 ** 4
 
+print("")
+print("Aritmeticos")
 print("Suma 5 + 5 = " , suma)
 print("Resta 7 - 2 = " , resta)
 print("Division 20 / 2 = " , division)
 print("Division Entera 20 // 3 = " , divisionEntera)
 print("Modulo 20 % 3 = " , modulo)
 print("Exponenciacion 2 ** 4 = " , exponeniacion)
+
+
+#Comparacion
+print("")
+print("COMPARACION")
+print("10 igual a 5 : ", 10 == 5)
+print("10 mayor igual a 5 : ", 10 >= 5)
+print("10 menor igual a 5 : ", 10 <= 5)
+print("10 mayor a 5 : ", 10 > 5)
+print("10 menor a 5 : ", 10 < 5)
+print("10 diferente a 5 : ", 10 != 5)
+
+#Logico
+print("")
+print("LOGICOS")
+print("10 mayor que 5 o 10 menor que 3: ", (10>5)or(10<3))
+print("10 mayor que 5 y 10 menor que 3: ", (10>5)and(10<3))
+print("negar que 10 mayor que 5: ", not(10>5))
