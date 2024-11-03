afe80596f0a57d555072d6c95bf3816745f9b3a7
ignacioTech <ignaciobarboza1983@gmail.com>
2024-11-03 10:12:34 -0300

    Create 3_estructuras_Control.py

---
+++ 3_estructuras_Control.py
@@ -0,0 +1,23 @@
+#Estructuras Condicionales
+# IF
+edad = 18
+if edad >=18:
+    print("Eres Mayor de edad, tienes: ", edad, " Años.")
+
+# If-else
+
+edad = 15
+if edad >=18:
+    print("Eres Mayor de edad, tienes: ", edad, " Años.")
+else:
+    print("Eres Menor de edad, tienes: ", edad, " Años.")
+
+#If-elif-else
+
+edad = 80
+if (edad >= 65) :
+    print("Eres Mayor de edad, tienes: ", edad, " Años.")
+elif edad >= 18:
+    print("Eres Mayor de edad, tienes: ", edad, " Años.")
+else:
+    print("Eres Menor de edad, tienes: ", edad, " Años.")
